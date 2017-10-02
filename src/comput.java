bd077e84dae9b9bc9eb3b2aef0a96714d2d95c44
linch <chlin@asia.edu.tw>
2017-10-02 09:46:23 +0800

    mi to km

---
+++ src/comput.java
@@ -0,0 +1,20 @@
+public class comput {
+
+    public static void main(String[] args) {
+        // 顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.println("兩個整數");
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int Sum = A+B;
+        int Sub = A-B;
+        int Mul = A*B;
+
+
+        System.out.println(Sum + " " + Sub + " " + Mul);
+
+    }
+
+}
