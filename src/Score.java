a1c1d6a0df0ed1aac89bb99a85b9cd8f42beb65d
linch <chlin@asia.edu.tw>
2017-10-02 09:49:37 +0800

    mi to km

---
+++ src/Score.java
@@ -0,0 +1,18 @@
+public class Score {
+
+    public static void main(String[] args) {
+        // 顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.println("請輸入英里");
+        double Score1= sc.nextDouble();
+        double Score2= sc.nextDouble();
+        double Score3= sc.nextDouble();
+
+        int Sum = Score1 + Score2 + Score3;
+        double Avg = Sum / 3;
+
+        System.out.println(Sum);
+        System.out.println(Avg);
+    }
+}
