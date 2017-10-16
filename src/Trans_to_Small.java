db53a3c7e8333c42eed8a7f6e60d296dd9d27038
linch <chlin@asia.edu.tw>
2017-10-16 10:19:34 +0800

    mi to km

---
+++ src/Trans_to_Small.java
@@ -0,0 +1,11 @@
+public class Trans_to_Small {
+
+    public static void main(String[] args) {
+        // 顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        System.out.println(str.toLowerCase());
+    }
+}
