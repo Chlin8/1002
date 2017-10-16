ae02b603779d7f9c930dbefd5eaaeeec40682a2c
linch <chlin@asia.edu.tw>
2017-10-16 10:49:35 +0800

    mi to km

---
+++ src/R_Unicode.java
@@ -0,0 +1,11 @@
+public class R_Unicode {
+
+    public static void main(String[] args) {
+        // 顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        char ch = sc.next().charAt(0);
+        System.out.println(Integer.toHexString(ch));
+
+    }
+}
