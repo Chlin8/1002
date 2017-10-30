f9c127c67fccc87a042800ddf14ba5523ea90a2c
I432 <chlin@asia.edu.tw>
2017-10-30 15:08:14 +0800

    1030

--- src/False_or_Pass.java
+++ src/False_or_Pass.java
@@ -5,14 +5,25 @@ public class False_or_Pass {
         java.util.Scanner sc = new java.util.Scanner(System.in);
 
         System.out.println("請輸入成績");
-        int grade= sc.nextInt();
+        int grade = sc.nextInt();
 
-        if (grade >=60){
-            System.out.println("成績及格：" + grade);
+        if((grade > 100) || (grade <0))
+        {
+
+            System.out.println("ERROR");
         }
         else
         {
-            System.out.println("成績不及格：" + grade);
+            if (grade >= 60)
+            {
+                System.out.println("成績及格：" + grade);
+            }
+            else
+            {
+                System.out.println("成績不及格：" + grade);
+            }
         }
+
+        System.out.println("End");
     }
 }
