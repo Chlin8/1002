f417bc57b0f93a7703c64f69b86712debfb5d122
I432 <chlin@asia.edu.tw>
2017-11-06 15:08:50 +0800

    11/6

---
+++ src/Max_num.java
@@ -0,0 +1,31 @@
+public class Max_num {
+
+    public static void main(String[] args) {
+        // 顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.println("請輸入三個數字");
+        Integer Num1= sc.nextInt();
+        Integer Num2= sc.nextInt();
+        Integer Num3= sc.nextInt();
+
+        if (Num1 > Num2){
+
+            if(Num1>Num3){
+                System.out.println(Num1 + " is the biggest number");
+            }
+            else{
+                System.out.println(Num3 + " is the biggest number");
+            }
+
+        }
+        else{
+            if(Num2 > Num3){
+                System.out.println(Num2 + " is the biggest number");
+            }
+            else{
+                System.out.println(Num3 + " is the biggest number");
+            }
+        }
+    }
+}
