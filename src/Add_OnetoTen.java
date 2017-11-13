39f296416631f9f4f8b550793c1544246f3feaf0
linch <chlin@asia.edu.tw>
2017-11-13 10:53:56 +0800

    mi to km

---
+++ src/Add_OnetoTen.java
@@ -0,0 +1,21 @@
+public class Add_OnetoTen {
+    public static void main(String[] args) {
+        // 顯示訊息
+
+        Integer i;
+        Integer Sum=0;
+        double Avg;
+
+        for(i=1;i<=10;i++)
+        {
+            System.out.print(i + "\n");
+            //Sum = Sum + i;
+
+        }
+
+        Avg = Sum /10 ;
+
+        System.out.println(Sum);
+        System.out.println(Avg);
+    }
+}
