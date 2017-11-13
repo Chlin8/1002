771cce899d810a8f3a6612aae203c34db4fc13a2
I432 <chlin@asia.edu.tw>
2017-11-13 15:59:21 +0800

    123132

--- src/while_addOnetoTen.java
+++ src/while_addOnetoTen.java
@@ -2,19 +2,29 @@ public class while_addOnetoTen {
 
     public static void main(String[] args) {
         // 顯示訊息
+        java.util.Scanner sc = new java.util.Scanner(System.in);
 
         Integer i;
         Integer Sum=0;
+        int num;
+
+        double avg;
 
         i=1;
-        while(Sum<500)
-        {
-            Sum = Sum + i;
-            i++;
-        }
+        System.out.println("請輸入第" + i + "個數字");
+        num = sc.nextInt();
 
+        while(num != 99999)
+        {
+            Sum = Sum + num;
+
+            i++;
+            System.out.println("請輸入第" + i + "個數字");
+            num = sc.nextInt();
+        }
+        avg = Sum / (i-1);
         System.out.println(Sum);
-        System.out.println(i-1);
+        System.out.println(avg);
     }
 
 }
