651a244b8724d391138dab4f691952259c59a7a1
I432 <chlin@asia.edu.tw>
2017-11-20 15:33:33 +0800

    1120

---
+++ src/star02.java
@@ -0,0 +1,19 @@
+public class star02 {
+    public static void main(String[] args) {
+
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int i,j;
+
+        for(j=1;j<=5;j++) {
+
+            for (i = 5; i >= j; i--) {
+                System.out.print("★");
+            }
+            System.out.print("\n");
+        }
+
+
+
+    }
+}
