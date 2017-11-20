7a641e5a3dcb0468abc33bc0c9c856db0c1c322d
I432 <chlin@asia.edu.tw>
2017-11-20 15:32:46 +0800

    1120

---
+++ src/Star01.java
@@ -0,0 +1,17 @@
+public class Star01 {
+    public static void main(String[] args) {
+
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        int i,j;
+
+        for (j = 1; j <= 10; j++) {
+
+            for (i = 1; i <= 6; i++) {
+                System.out.print("★");
+            }
+            System.out.print("\n");
+        }
+
+    }
+}
