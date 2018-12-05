f698a7fe773aca66569cc580bb28c64029926ca5
linch <chlin@asia.edu.tw>
2018-12-05 11:42:56 +0800

    mi to km

---
+++ src/shift_letter.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class shift_letter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+        int n = Integer.parseInt(in.nextLine());
+        char str[] = message.toCharArray();
+
+        for(int i=0;i<str.length;i++)
+        {
+            if(str[i]>'A' && str[i]<'Z')
+            {
+                str[i]=(char)(((str[i] - 'A')+n)%26+'A');
+            }
+            else
+            {
+                if (str[i]>'a' && str[i]<'z')
+                {
+                    str[i]=(char)(((str[i] - 'a')+n)%26+'a');
+                }
+                else
+                {
+                    if (str[i]>'0' && str[i]<'9')
+                    {
+                        str[i]=(char)(((str[i] - '0')+n)%10+'0');
+                    }
+                }
+
+            }
+
+            System.out.print(str[i]);
+        }
+
+        System.out.println();
+    }
+}
